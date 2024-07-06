dc4320d92c5be2fe10471a8dcadec87b734730ab
sleepyts <choujack373@gamil.com>
2024-07-06 10:56:14 +0800

    添加音频

--- src/App.vue
+++ src/App.vue
@@ -41,16 +41,38 @@ const noises = [
     name: "大雨",
     fileName: "/audio/rain.mp3",
   },
+  {
+    name: "雨打树叶",
+    fileName: "/audio/rain-on-leaves.mp3",
+  },
+  {
+    name: "水下",
+    fileName: "/audio/underwater.mp3",
+  },
+  {
+    name: "细雨",
+    fileName: "/audio/drizzle.mp3",
+  }
 
 ]
 </script>
 
 <template>
-  <div v-for="noise in noises" :key="noise.name">
-    <noise-player :noise="noise"></noise-player>
+  <div class="noise-player-container-wrapper">
+    <div v-for="noise in noises" :key="noise.name" class="noise-player-container">
+      <noise-player :noise="noise"></noise-player>
+    </div>
   </div>
+
 </template>
 
 <style scoped>
-
+.noise-player-container{
+  width: calc(33% - 10px);
+  margin-left: 10px;
+}
+.noise-player-container-wrapper{
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
--- src/components/noisePlayer.vue
+++ src/components/noisePlayer.vue
@@ -26,7 +26,7 @@ watch(isPlaying, (newVal, oldVal) => {
 </script>
 
 <template>
-  <div class="noise-player">
+  <div>
     <h1>{{props.noise.name}}</h1>
     <audio :id="props.noise.name"  controls loop style="display:none"></audio>
     <a-switch v-model:checked="isPlaying" />
@@ -36,8 +36,5 @@ watch(isPlaying, (newVal, oldVal) => {
 </template>
 
 <style scoped>
-.noise-player {
-  width: 70%;
-  margin: 0 auto;
-}
+
 </style>
